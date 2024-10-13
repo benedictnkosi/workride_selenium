3635269f4ba2f4874cb3be295ac88df90ada7825
BenedictNkosi01 <Benedict.nkosi@merlinentertainments.biz>
2024-10-13 21:24:58 +0200

    driver

--- src/main/java/org/workrideclub/MapsPage.java
+++ src/main/java/org/workrideclub/MapsPage.java
@@ -17,6 +17,7 @@ public class MapsPage {
             By by = By.xpath(
                     "//span[@id='section-directions-trip-travel-mode-0']/following-sibling::div//div[contains(@class,'fontHeadlineSmall')]");
             time = driver.findElement(by).getText();
+            driver.quit();
             return time;
         } catch (Exception e) {
             System.out.println("No time found");
--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -15,7 +15,7 @@ public class RunTest {
             Utils.addNewDriverTravelTime();
             Utils.processFirstUnmatched();
             try {
-                Thread.sleep(10000);
+                Thread.sleep(60000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
